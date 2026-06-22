ef2f6b00a1c06c536e5b55e39cb499ac25c22645
user <user@gmail.com>
2026-06-22 13:55:51 +0600

    fresh gen spring boot project for jasper report

---
+++ src/main/java/com/fifa/reporting/worldcup_report_app/WorldcupReportAppApplication.java
@@ -0,0 +1,13 @@
+package com.fifa.reporting.worldcup_report_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupReportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupReportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fifa/reporting/worldcup_report_app/WorldcupReportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifa.reporting.worldcup_report_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupReportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
